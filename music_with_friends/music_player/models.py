eaedd6c950edcec3710a5c7df41adf30ab3fc084
Jeremy Becker <beckerjeremy@outlook.com>
2019-06-28 09:35:42 +0200

    Added models

--- music_with_friends/music_player/models.py
+++ music_with_friends/music_player/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=20)
+
+class Position(models.Model):
+    # time when the video was played the last time
+    time = models.TimeField(auto_now=True, auto_now_add=False)
+    # seconds in the video when it was played
+    time_in_video = models.models.IntegerField()
+
+class QueueEntry(models.Model):
+    video_url = models.URLField(max_length=200)
+
+class Room(models.Model):
+    name = models.CharField(max_length=20)
+    users = models.ArrayModelField(model_container=QueueEntry)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    queue = models.ArrayModelField(model_container=QueueEntry)
