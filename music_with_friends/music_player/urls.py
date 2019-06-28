f1e3a381f761b001eb66f72f5c5ae7cfa4d588c3
Silvan Kisseleff <silvan.kisseleff@gmail.com>
2019-06-28 09:14:07 +0200

    added dashboard view and url

--- music_with_friends/music_player/urls.py
+++ music_with_friends/music_player/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='music_player-home'),
     path('home/', views.home, name='music_player-home'),
-    path('login/', views.login, name='music_player-login')
+    path('login/', views.login, name='music_player-login'),
+    path('dashboard/', views.dashboard, name='music_player-dashboard')
 ]
